555cab740fea960aff75a099fb127d732e322340
kochoa-dot <70181138+kochoa-dot@users.noreply.github.com>
2023-04-28 19:42:58 -0600

    creating the conexion between frontend and backend

--- InventoryApp/src/main/java/com/tukicoders/InventoryApp/controller/CUserController.java
+++ InventoryApp/src/main/java/com/tukicoders/InventoryApp/controller/CUserController.java
@@ -3,14 +3,12 @@ package com.tukicoders.InventoryApp.controller;
 import com.tukicoders.InventoryApp.model.CUser;
 import com.tukicoders.InventoryApp.repository.CUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 public class CUserController {
 
     @Autowired

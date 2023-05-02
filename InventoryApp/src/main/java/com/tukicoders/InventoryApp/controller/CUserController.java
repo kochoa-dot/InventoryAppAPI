6a26c93def38f70cbc16c0b8ecaca7b8a9546eb2
kochoa-dot <70181138+kochoa-dot@users.noreply.github.com>
2023-05-02 16:50:56 -0600

    Adding the final endpoints and exceptions for users

--- InventoryApp/src/main/java/com/tukicoders/InventoryApp/controller/CUserController.java
+++ InventoryApp/src/main/java/com/tukicoders/InventoryApp/controller/CUserController.java
@@ -1,5 +1,6 @@
 package com.tukicoders.InventoryApp.controller;
 
+import com.tukicoders.InventoryApp.exception.UserNotFoundException;
 import com.tukicoders.InventoryApp.model.CUser;
 import com.tukicoders.InventoryApp.repository.CUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,30 @@ public class CUserController {
     @GetMapping("/users")
     List<CUser> getAllUsers(){
         return cUserRepository.findAll();
+    }
+
+    @GetMapping("/user/{id}")
+    CUser getUserById(@PathVariable Long id){
+        return cUserRepository.findById(id).orElseThrow(() -> new UserNotFoundException(id));
+    }
+
+    @PutMapping("/user/{id}")
+    CUser updateUser(@RequestBody CUser newUser, @PathVariable Long id){
+        return cUserRepository.findById(id)
+                .map(user -> {
+                    user.setUsername(newUser.getUsername());
+                    user.setName(newUser.getName());
+                    user.setEmail(newUser.getEmail());
+                    return cUserRepository.save(user);
+        }).orElseThrow(()-> new UserNotFoundException(id));
+    }
+
+    @DeleteMapping("/user/{id}")
+    String deleteUser(@PathVariable Long id){
+        if (!cUserRepository.existsById(id)){
+            throw new UserNotFoundException(id);
+        }
+        cUserRepository.deleteById(id);
+        return "User with id "+id+ " has been deleted success.";
     }
 }
